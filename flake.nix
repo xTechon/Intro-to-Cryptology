cadd65d7be71074ae5149162d6adb554771d188e
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-02-07 17:50:16 -0500

    Update flake for development

--- flake.nix
+++ flake.nix
@@ -2,36 +2,72 @@
 {
   description = "LaTeX Document Demo";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-vscode-extensions }:
     with flake-utils.lib; eachSystem allSystems (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-basic latex-bin latexmk;
-      };
-    in rec {
-      packages = {
-        document = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "latex-demo-document";
-          src = self;
-          buildInputs = [ pkgs.coreutils tex ];
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-          buildPhase = ''
-            export PATH="${pkgs.lib.makeBinPath buildInputs}";
-            mkdir -p .cache/texmf-var
-            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-              latexmk -interaction=nonstopmode -pdf -lualatex \
-              document.tex
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp document.pdf $out/
+      let
+        pkgs = import nixpkgs {inherit system; config.allowUnfree = true; };
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-small latex-bin latexmk texcount;
+        };
+        extensions = nix-vscode-extensions.extensions.${system};
+
+        codium = pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
+          vscodeExtensions = [
+            extensions.open-vsx.bbenoist.nix
+            extensions.open-vsx.jnoortheen.nix-ide
+            extensions.open-vsx.asvetliakov.vscode-neovim
+            extensions.open-vsx.zokugun.explicit-folding
+
+            extensions.open-vsx.efoerster.texlab
+            extensions.open-vsx.tecosaur.latex-utilities
+            extensions.open-vsx.james-yu.latex-workshop
+          ];
+        };
+
+        HW1Shell = pkgs.mkShell.override {} {
+          packages = [
+            codium
+            pkgs.texlab # lsp
+            tex # needed for development compilation
+            pkgs.perl # needed for latex-workshop
+          ];
+          shellHook = ''
+            export LABEL="HW1"
           '';
         };
-      };
-      defaultPackage = packages.document;
-    });
+
+      in
+      rec {
+        packages = {
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "latex-demo-document";
+            src = self;
+            buildInputs = [ pkgs.coreutils tex ];
+            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              mkdir -p .cache/texmf-var
+              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                latexmk -interaction=nonstopmode -pdf -lualatex \
+                HW1/HW1.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp document.pdf $out/
+            '';
+          };
+        };
+        defaultPackage = packages.document;
+
+        devShells = {
+          default = HW1Shell;
+          HW1 = HW1Shell;
+        };
+
+      });
 }
